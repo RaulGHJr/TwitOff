757235a9d6af4cd3ac2ea450b516da72345958fa
cicbeast <49789241+cicbeast@users.noreply.github.com>
2019-10-02 23:50:19 -0400

    modified model and app

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from .model import DB
+
 
 def create_app():
     '''Create and configure an instance of the Flask application.'''
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/cicbeast/TwitOff/twitoff/db.sqlite3'
+    DB.init_app(app)
 
-    @app.route('/')
+    @app.route('/hello')
     def root():
-        return 'Hello TwitOff!'
+        return '<h2> Welcome to TwitOff! </h2>'
 
     return app 
