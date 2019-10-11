1b040036a4f0698f32ee559ea5f172e5e65893fc
cicbeast <49789241+cicbeast@users.noreply.github.com>
2019-10-10 23:23:06 -0400

    Added Proc file ad updated twitoff and pipfile to include gunicorn

--- twitoff/model.py
+++ twitoff/model.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
+
 class User(DB.Model):
     '''Twitter users that we query and store historical tweets'''
     id = DB.Column(DB.BigInteger, primary_key=True)
@@ -11,10 +12,11 @@ class User(DB.Model):
     # Tweet IDS are ordinal ints, so we can fetch most recent tweets
     newest_tweet_id = DB.Column(DB.BigInteger, nullable=False)
 
+
 class Tweets(DB.Model):
     '''Stores tweets'''
     id = DB.Column(DB.BigInteger, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(300))
     embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -3,7 +3,8 @@ import tweepy
 from decouple import config
 from .model import DB, Tweets, User
 
-TWITTER_USERS =  ['elonmusk', 'nasa', 'sadserver', 'austen', 'lockheedmartin', 'lizzo']
+TWITTER_USERS = ['elonmusk', 'funnyordie', 'nasa', 'sadserver', 'austen',
+                 'neiltyson', 'lizzo', 'bigdata', 'buzzfeed']
 
 TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_API_KEY'),
                                    config('TWITTER_CONSUMER_API_SECRET'))
@@ -16,38 +17,39 @@ BASILICA = basilica.Connection(config('BASILICA_KEY'))
 def add_or_update_user(username):
     """Add a user and their tweets to database."""
     try:
-        # Get user info from tweepy API    
+        # Get user info from tweepy API
         twitter_user = TWITTER.get_user(username)
 
         # Add user info to User table in database
         db_user = (User.query.get(twitter_user.id) or
-                    User(id=twitter_user.id, 
-                       username=twitter_user.screen_name,
-                       followers=twitter_user.followers_count))                       
+                   User(id=twitter_user.id,
+                        username=username,
+                        followers=twitter_user.followers_count))
         DB.session.add(db_user)
-        
+
         # Add as many recent non-retweet/reply tweets as possible
         # 200 is a Twitter API limit for single request
-        tweets = twitter_user.timeline(count=200, 
-                                       exclude_replies=True, 
-                                       include_rts=False, 
+        tweets = twitter_user.timeline(count=200,
+                                       exclude_replies=True,
+                                       include_rts=False,
                                        tweet_mode='extended',
-                                       since_id=db.user.newest_tweet_id)
-        
+                                       since_id=db_user.newest_tweet_id)
+
         # Add additional user info to User table in database
         if tweets:
             db_user.newest_tweet_id = tweets[0].id
 
-        # Loop over each tweet 
+        # Loop over each tweet
         for tweet in tweets:
 
             # Get Basilica embedding for each tweet
-            embedding = BASILICA.embed_sentence(tweet.full_text, 
+            embedding = BASILICA.embed_sentence(tweet.full_text,
                                                 model='twitter')
             # Add tweet info to Tweets table in database
-            db_tweet = Tweets(id=tweet.id, 
+            db_tweet = Tweets(id=tweet.id,
                               text=tweet.full_text[:300],
                               embedding=embedding)
+            db_user.tweets.append(db_tweet)
             DB.session.add(db_tweet)
 
     except Exception as e:
@@ -55,9 +57,9 @@ def add_or_update_user(username):
         raise e
     else:
         DB.session.commit()
-    
 
-def add_users(users=TWITTER_USERS):
+
+def add_default_users(users=TWITTER_USERS):
     """
     Add/update a list of users (strings of user names).
     May take awhile, so run 'offline' (flask shell).
@@ -69,4 +71,4 @@ def add_users(users=TWITTER_USERS):
 def update_all_users():
     """Update all Tweets for all Users in the User table. """
     for user in User.query.all():
-        add_or_update_user(user.name)
+        add_or_update_user(user.username)
